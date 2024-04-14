2289ceba14b0fcb4ed48109836790912231d34cd
Aditya Sakpal <112710558+Aditya-Sakpal@users.noreply.github.com>
2024-04-14 21:48:47 +0530

    Update db.ts

--- apps/backend/src/db.ts
+++ apps/backend/src/db.ts
@@ -1,6 +1,4 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
-dotenv.config({ path: '../.env' });
 
 const { NEXT_PUBLIC_MONGO_URL }: { NEXT_PUBLIC_MONGO_URL?: string | undefined } = process.env as { NEXT_PUBLIC_MONGO_URL?: string | undefined };
 
